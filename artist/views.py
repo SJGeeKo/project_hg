78b4d86bad8b0dc9de0e6bb4c4dccbe6835ca6a2
SJGeeKo <sungjungeeko@gmail.com>
2018-10-15 14:30:21 +0900

    아티스트 디테일페이지 slick 제거, 작품설명 및 아티스트 디테일페이지에 네임카드 추가

--- artist/views.py
+++ artist/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from shop.models import Painter, Painting
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 # Create your views here.
 def artistDetailView(request, artist_slug):
-    paintings = Painting.objects.all().filter(painter__slug__exact = artist_slug)
+    paintings_list = Painting.objects.all().filter(painter__slug__exact = artist_slug)
+    '''
+    Paginator
+    '''
+    paginator = Paginator(paintings_list, 16)
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+    try:
+        paintings = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        paintings = paginator.page(paginator.num_pages)
     return render(request, 'artist/detail.html', {'paintings': paintings})
--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,7 @@ class Painter(models.Model):
     slug = models.CharField(max_length=250, unique=True)
     birth = models.DateField()
     description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='user_profile', blank=True)
 
     class Meta:
         ordering = ('name',)
@@ -18,6 +19,7 @@ class Painter(models.Model):
 class Painting(models.Model):
     name = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
+    material = models.CharField(max_length = 250, default='Oil')
     description = models.TextField(blank=True)
     painter = models.ForeignKey(Painter, on_delete=models.CASCADE)
     width = models.IntegerField(default=10)
